b5432248023fb3754f40b5409fd0b2c4d952e064
nickyyy28 <a3092989305@163.com>
2023-07-26 21:51:37 +0800

    game crash

---
+++ src/main/java/com/nickyyy/testfabric/mixin/MixinTitle.java
@@ -0,0 +1,18 @@
+package com.nickyyy.testfabric.mixin;
+
+import com.nickyyy.testfabric.util.ModLog;
+import net.minecraft.client.gui.screen.TitleScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.lang.reflect.Method;
+
+//@Mixin(TitleScreen.class)
+public class MixinTitle {
+//    @Inject(method = "init()V", at = @At("HEAD"))
+    private void InjectMethod(CallbackInfo ci) {
+//        ModLog.LOGGER.info("this is the mixin log");
+    }
+}
